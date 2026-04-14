45a76ea3ad4fa0f5065a96d37b1c22fedd59e987
HarshitAggarwal10 <harshitaggarwal100306@gmail.com>
2026-04-15 03:20:00 +0530

    chore: Initialize frontend project with Vite and Tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2874f0',
+        secondary: '#fbbf24',
+      },
+    },
+  },
+  plugins: [],
+}
